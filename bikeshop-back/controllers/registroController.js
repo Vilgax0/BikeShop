23f578451f59cf19bfe1cf13ac6ba42dd48a3789
Vilgax0 <mkclaren.tobar@upb.edu.co>
2024-11-05 00:01:04 -0500

    actualizacion registro U Nuevos

--- bikeshop-back/controllers/registroController.js
+++ bikeshop-back/controllers/registroController.js
@@ -4,37 +4,37 @@ const usersRef = db.collection('usuarios');
 
 async function registro(req, res) {
   try {
-    const { name, email, password} = req.body;
+    const { name, email, password } = req.body;
 
-    if(!email || !password || !name){
-      return res.status(400).json({message: "Faltan datos"});
+    if (!email || !password || !name) {
+      return res.status(400).json({ message: "Faltan datos" });
     }
+
+    // Verifica si el nombre de usuario ya está en uso
     const nameExists = await usersRef.where('usuario', '==', name).get();
 
     if (!nameExists.empty) {
       return res.status(400).send({ message: 'El nombre de usuario ya está en uso' });
     }
 
+    // Crea el usuario en Firebase Authentication
     const userRecord = await admin.auth().createUser({
       email,
       password,
       displayName: name,
     });
 
+    // Almacena información del usuario en Firestore
     await usersRef.doc(userRecord.uid).set({
       email,
       usuario: name,
     });
 
-
     res.status(201).send({ message: 'Usuario registrado exitosamente' });
   } catch (error) {
-    console.error(error);
+    console.error("Error al registrar el usuario:", error);
     res.status(500).send({ message: 'Error al registrar el usuario' });
   }
 }
 
 module.exports = registro;
-
-
-
